a3053a8aaeb36a7744c535d87406eff38789c9c0
Evan <eklein217@gmail.com>
2018-06-17 17:36:27 -0400

    attempting heroku again...

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 // server.js
 const express = require('express');
 const app = express();
+const path=expres();
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
+
+app.get('*', function(req, res) {
+    res.sendfile('./server/views/index.html')
+  })
