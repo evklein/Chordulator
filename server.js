3e6fff3e8a6edf18fcc15ec3e310d2fe3816454d
Evan <eklein217@gmail.com>
2018-06-17 17:47:14 -0400

    configuring server

--- server.js
+++ server.js
@@ -10,5 +10,5 @@ app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
 
 app.get('*', function(req, res) {
-    res.sendfile('./server/views/index.html')
+    res.sendfile('./src/index.html')
   })
